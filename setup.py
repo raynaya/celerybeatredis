1e5303d2b293aa97e71ace4f1830d7bf1a4f9a98
Shamik Ray <shamik.ray@gdn-commerce.com>
2016-10-19 20:00:45 +0530

    Modified Setup and celery config"

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from setuptools import setup
 setup(
     name = "celerybeat-redis",
     description = "A Celery Beat Scheduler that uses Redis to store both schedule definitions and status information",
-    version = "0.1.5",
+    version = "0.1.6",
     license = "Apache License, Version 2.0",
     author = "Kong Luoxing",
     author_email = "kong.luoxing@gmail.com",
-    home_page = 'https://github.com/kongluoxing/celerybeatredis',
-    maintainer = "Kong Luoxing",
-    maintainer_email = "kong.luoxing@gmail.com",
+    home_page = 'https://github.com/raynaya/celerybeatredis/',
+    maintainer = "Shamik Ray",
+    maintainer_email = "shamik.ray@coviam.com",
 
     keywords = "python celery beat redis",
 
@@ -20,7 +20,7 @@ setup(
     install_requires=[
         'setuptools',
         'redis>=2.10.3',
-        'celery>=3.1.16'
+        'celery>=3.1.23'
     ]
 
 )
