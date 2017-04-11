207540a3654ec8828d3cfead4d70cc170192407e
Shamik Ray <shamik.ray@gdn-commerce.com>
2017-04-11 11:35:32 +0530

    bug fix

--- celerybeatredis/schedulers.py
+++ celerybeatredis/schedulers.py
@@ -262,7 +262,7 @@ class RedisScheduler(Scheduler):
             else:
                 #if the schdule is already present compare it for changes in schdule
                 original_schedule_obj=(json.loads(self.rdb.get("tasks:meta:"+name).decode('utf-8')))['schedule']
-                new_schedule_obj=self.schedule[name]['schedule']
+                new_schedule_obj=json.loads(self.schedule[name].jsondump())['schedule']
                 #compare the schedules inside the two
                 if original_schedule_obj != new_schedule_obj:
                     print('Adding Modified Task: tasks:meta:{}'.format(name))
